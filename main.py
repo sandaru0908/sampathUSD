3dc557f36d18bf155783a79d6d99fda38987f807
sandaru0908 <88138452+sandaru0908@users.noreply.github.com>
2026-05-26 12:51:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-import requests
+from playwright.sync_api import sync_playwright
+import json
+
+URL = "https://www.sampath.lk/api/exchange-rates"
 
 # Google Apps Script Web App URL
 GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbyNSUJgXxvXRmn9fbr641poBZGS6jr3u4HWd9SXf1QQLb2wmTfGaI0uOAgvZUvvSYk/exec"
 
 
 def get_usd_ttbuy():
-    try:
-        r = requests.get("https://www.sampath.lk/api/exchange-rates", timeout=10)
-        r.raise_for_status()
-        data = r.json()
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+
+        context = browser.new_context(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120"
+        )
+
+        page = context.new_page()
+
+        response = page.goto(URL, wait_until="domcontentloaded")
+
+        text = response.text()
+        browser.close()
+
+        data = json.loads(text)
 
         for item in data["data"]:
             if item["CurrCode"] == "USD":
                 return item["TTBUY"]
 
-    except Exception as e:
-        print("Error fetching exchange rate:", e)
-
     return None
 
 
 def send_to_sheet(value):
+    import requests
+
     try:
         response = requests.get(GOOGLE_SHEET_URL, params={"value": value}, timeout=10)
         print("Sheet response:", response.text)
