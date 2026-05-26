9a5998960fa193b6c37303a29ed997da43f7f00f
sandaru0908 <88138452+sandaru0908@users.noreply.github.com>
2026-05-26 13:02:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from playwright.sync_api import sync_playwright
-import json
 import requests
 
 URL = "https://www.sampath.lk/api/exchange-rates"
-GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbyNSUJgXxvXRmn9fbr641poBZGS6jr3u4HWd9SXf1QQLb2wmTfGaI0uOAgvZUvvSYk/exec"
+SHEET = "https://script.google.com/macros/s/AKfycbyNSUJgXxvXRmn9fbr641poBZGS6jr3u4HWd9SXf1QQLb2wmTfGaI0uOAgvZUvvSYk/exec"
 
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
-    page.goto(URL, wait_until="networkidle")
 
-    data = json.loads(page.text_content("body"))
+    response = page.goto(URL, wait_until="domcontentloaded")
+    data = response.json()
+
     browser.close()
 
     for item in data["data"]:
         if item["CurrCode"] == "USD":
             value = item["TTBUY"]
-            print("USD TTBUY:", value)
+            print("USD:", value)
 
-            requests.get(GOOGLE_SHEET_URL, params={"value": value})
+            requests.get(SHEET, params={"value": value})
             break
